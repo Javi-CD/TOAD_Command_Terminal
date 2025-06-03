f0f6ecba418a7c56701b40847ea15ac932622053
Javier <javierperezviloria07@gmail.com>
2025-06-03 14:51:49 -0500

    feat(gui): implement REPL-style input and output history in terminal window

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,15 @@
-mod commands;
+mod app;
 
-use std::io::{self, Write};
+use app::terminal::ToadTerminalApp;
+use eframe::egui;
 
-fn main() {
-    println!("Welcome to the Toad terminal, write \"Help\" to see the available commands ..");
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
 
-    loop {
-        print!("Toad> ");
-        io::stdout().flush().unwrap(); // Ensure the prompt is printed immediately
-
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-
-        let input = input.trim();
-        if input.is_empty() {
-            continue; // Skip empty input
-        };
-
-        let parts: Vec<&str> = input.split_whitespace().collect();
-        let command = parts[0];
-        let args = &parts[1..].to_vec();
-
-        match command.to_lowercase().as_str() {
-            "cd" => commands::cd::change_directory(args.clone()),
-            "ls" => commands::ls::list_directory(args.clone()),
-            "clear" => commands::clear::clear_screen(args.clone()),
-            "echo" => commands::echo::echo(args.clone()),
-            "pwd" => commands::pwd::print_working_directory(args.clone()),
-            "help" => commands::help::display_help(args.clone()),
-            "version" => commands::version::execute(args.clone()),
-            "exit" => {
-                println!("Exiting Toad terminal...");
-                break;
-            }
-            _ => println!(
-                "Unknown command: {}. Type 'help' for a list of available commands.",
-                command
-            ),
-        }
-    }
+    // Set the initial window size
+    eframe::run_native(
+        "Toad Terminal",
+        options,
+        Box::new(|_cc| Box::new(ToadTerminalApp::default())),
+    )
 }
